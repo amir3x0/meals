5560368fdb826d25227aa97cd85a001c8c5a6918
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2025-08-16 20:13:48 +0300

    categories go up

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,17 +5,43 @@ import 'package:meals/screens/meals.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/models/category.dart' as models;
 
-
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.avalibaleMeals});
 
   final List<Meal> avalibaleMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, models.Category category) {
-  // Respect active filters by using the pre-filtered list provided from TabsScreen
-  final fliteredmeals = avalibaleMeals
-    .where((meal) => meal.categories.contains(category.id))
-    .toList();
+    // Respect active filters by using the pre-filtered list provided from TabsScreen
+    final fliteredmeals = widget.avalibaleMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
 
     Navigator.of(context).push(MaterialPageRoute(
       builder: (ctx) => MealsScreen(
@@ -27,8 +53,9 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
         padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -38,12 +65,17 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final category in dummyCategories)
-            CategoryGridItem(category: category, onSelectCategory:(id) {
-              _selectCategory(context, category);
-            }
-          ),
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: (id) {
+                  _selectCategory(context, category);
+                }),
         ],
       ),
+      builder: (ctx, child) => Padding(
+          padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+          child: child,
+        ),
     );
   }
 }
